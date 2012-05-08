9bf60f0359c719e7a40c614c66cc4ba5cefbf56a
Mike Tigas <mike@tig.as>
2012-05-08 16:24:59 -0700

    obfsproxy src dists no longer found in tarball format, bumping down to using snapshot of git tag in gitweb.torproject.org, which is less than ideal

--- Formula/obfsproxy.rb
+++ Formula/obfsproxy.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Obfsproxy < Formula
   homepage 'https://www.torproject.org/'
-  version "0.1.3"
-  url 'https://www.torproject.org/dist/obfsproxy/obfsproxy-0.1.3.tar.gz'
-  sha256 '15fd924f39d0ffd46e2efc5f594f6a264acea7e3dd2b4b716db2d73d684d088c'
+  version "0.1.4"
+  url 'hhttps://gitweb.torproject.org/obfsproxy.git/snapshot/94ebc4c3edf1e3e5f313444e59981ac557578df5.tar.gz'
+  sha256 '475717e63531201cce5903a70b9ffe8918a03b4050b4dcd75850f6937c1ced9d'
   head 'git://git.torproject.org/git/obfsproxy.git'
 
   depends_on 'libevent'
