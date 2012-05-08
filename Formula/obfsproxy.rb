35d66a61cf4b5d955d08193ab171cfc657091695
Mike Tigas <mike@tig.as>
2012-05-08 16:25:58 -0700

    typo'd the url, i am an idiot

--- Formula/obfsproxy.rb
+++ Formula/obfsproxy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Obfsproxy < Formula
   homepage 'https://www.torproject.org/'
   version "0.1.4"
-  url 'hhttps://gitweb.torproject.org/obfsproxy.git/snapshot/94ebc4c3edf1e3e5f313444e59981ac557578df5.tar.gz'
+  url 'https://gitweb.torproject.org/obfsproxy.git/snapshot/94ebc4c3edf1e3e5f313444e59981ac557578df5.tar.gz'
   sha256 '475717e63531201cce5903a70b9ffe8918a03b4050b4dcd75850f6937c1ced9d'
   head 'git://git.torproject.org/git/obfsproxy.git'
 
