156214a0ca7a02c3c8080ef8cdde162e12e0adb8
Mike Tigas <mike@tig.as>
2013-05-21 22:21:42 -0400

    0.2.4.12-alpha

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Tor < Formula
   homepage 'https://www.torproject.org/'
-  version "0.2.4.11"
-  url 'https://archive.torproject.org/tor-package-archive/tor-0.2.4.11-alpha.tar.gz'
-  sha256 '2297afbfaa969147f7080935b89bcbbe28f9dd50e907dd70e931ea940bbb9902'
+  version "0.2.4.12"
+  url 'https://archive.torproject.org/tor-package-archive/tor-0.2.4.12-alpha.tar.gz'
+  sha256 '29445d559f1c3097bc0513c94d4aeb22f4bc93ab6ef7dbff5104f7bc8d8e677c'
   head 'git://git.torproject.org/git/tor.git'
 
   option 'with-upnp', 'Enable upnp "PortForwarding" option.'
